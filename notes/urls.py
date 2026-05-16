17bb76d9c0468fea80c1871404a7528af67d0bbc
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-16 13:13:48 +0500

    Update urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('notes.urls')),
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
+
+    path(
+        "",
+        include(("notes.urls", "notes"), namespace="notes"),
+    ),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
+
+# Customize Django admin panel
+admin.site.site_header = "SmartNotes Admin"
+admin.site.site_title = "SmartNotes Portal"
+admin.site.index_title = "Welcome to SmartNotes Dashboard"
